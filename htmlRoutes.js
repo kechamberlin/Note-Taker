93071498681a3516657446e58de9c2efde8adaa3
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2019-10-21 22:19:54 -0700

    Changed app.get route to notes instead of reserve in htmlRoutes.html file.  Rearranged contents on index.html and notes.html files to now include current notes on home page, and a separate page to make a new note

--- htmlRoutes.js
+++ htmlRoutes.js
@@ -15,12 +15,8 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
-  app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/tables.html"));
-  });
-
-  app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/reserve.html"));
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/notes.html"));
   });
 
   // If no matching route is found default to home
